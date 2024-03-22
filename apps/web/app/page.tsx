dce76b9e5a66214711dfe174db0f61682753fa21
devodii <emmanuelodii80@gmail.com>
2024-03-22 20:22:58 +0100

    perf: simplfy tailwind setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@repo/ui/button";
+
 export default function Page(): JSX.Element {
-  return <div className="text-red-500">Home page</div>;
+  return (
+    <div className="text-red-500">
+      Home page
+      <Button>Hello world</Button>
+    </div>
+  );
 }
