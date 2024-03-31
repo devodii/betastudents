b84ce1ae9034eeaa55ac61f0adad3cc5bd2faaa9
devodii <emmanuelodii80@gmail.com>
2024-03-31 21:39:21 +0100

    chore: fix actions

--- apps/web/actions/external.ts
+++ apps/web/actions/external.ts
@@ -1,10 +1,14 @@
 "use server";
 
 export const getCountries = async () => {
-  const response = await fetch(
-    "https://restcountries.com/v3.1/all?fields=name,flags"
-  );
-  const countries = await response.json();
+  try {
+    const response = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags"
+    );
+    const countries = await response.json();
 
-  return countries;
+    return countries;
+  } catch (error) {
+    console.error({ error });
+  }
 };
